54274928e656abb3423d2fa307a724a623cc5b30
emacsun <emacsun@163.com>
2018-02-19 16:58:57 +0800

    trivial update of .spacemacs.d
    change the org-headline face

--- layers/zclorgexport/config.el
+++ layers/zclorgexport/config.el
@@ -1,7 +1,9 @@
 (with-eval-after-load 'org
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;wo de;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-  (setq org-src-fontify-natively t) ;设置代码高亮
+  (setq org-src-fontify-natively t)
+  ;;automatically delete this auxiliary files after export
 
+  (setq org-latex-logfiles-extensions (quote ("lof" "lot" "tex~" "aux" "idx" "log" "out" "toc" "nav" "snm" "vrb" "dvi" "fdb_latexmk" "blg" "brf" "fls" "entoc" "ps" "spl" "bbl")))
 ;;;;;;;;;;;;;;;;;;;;;;;;org export latex;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   ;;need to add #+LaTeX_CLASS at the beginning
   (setq org-list-allow-alphabetical t)
