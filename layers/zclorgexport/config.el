f8e18d5d5c7b8e2cb070ca888f6d28a7e887850f
emacsun <emacsun@163.com>
2018-02-21 13:48:06 +0800

    add pandoc layer and support export .org as .docx
    pandoc is a swiss-army knife for file format conversion
    
    also add the following code snippet to open the outputing file with the default
    apps.
    
     (setq org-file-apps
           '(("\\.docx\\'" . default)
             ("\\.mm\\'" . default)
             ("\\.x?html?\\'" . default)
             ("\\.pdf\\'" . default)
             (auto-mode . emacs)))

--- layers/zclorgexport/config.el
+++ layers/zclorgexport/config.el
@@ -1,9 +1,17 @@
 (with-eval-after-load 'org
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;wo de;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   (setq org-src-fontify-natively t)
+  (setq org-hide-emphasis-markers t)
   ;;automatically delete this auxiliary files after export
 
   (setq org-latex-logfiles-extensions (quote ("lof" "lot" "tex~" "aux" "idx" "log" "out" "toc" "nav" "snm" "vrb" "dvi" "fdb_latexmk" "blg" "brf" "fls" "entoc" "ps" "spl" "bbl")))
+  ;; open docx files in default application (ie msword)
+  (setq org-file-apps
+        '(("\\.docx\\'" . default)
+          ("\\.mm\\'" . default)
+          ("\\.x?html?\\'" . default)
+          ("\\.pdf\\'" . default)
+          (auto-mode . emacs)))
 ;;;;;;;;;;;;;;;;;;;;;;;;org export latex;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   ;;need to add #+LaTeX_CLASS at the beginning
   (setq org-list-allow-alphabetical t)
